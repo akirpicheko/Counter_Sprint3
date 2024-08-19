4656955e733072bce8956d8f6c3932f5de8ae8ef
akirpichenko <nastya.8@mail.ru>
2024-08-19 23:42:06 +0300

    Complete

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,60 +8,56 @@
 import UIKit
 import Foundation
 
-//Значение счётчика
-var currentValue: Int = 0
-
-//Отдаёт текущую дату и время
-func currentTime() -> String{
-    var time = Date()
-    var dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "[dd-MM-yyyy HH:mm:ss]"
-    var currentTime = dateFormatter.string(from: time)
-    return currentTime
-}
-
 //Возврат значения лога
-enum loggingAction: String{
+enum LoggingAction: String {
     case increase = ": значение изменено на +1"
     case decrease = ": значение изменено на -1"
     case reset = ": значение сброшено"
     case nagativeValue = ": попытка уменьшить значение счётчика ниже 0"
 }
 
-//Массив куда вписывается журнал действий
-var historyArray: [String] = []
-
-//Добвляем историю в журнал
-func updateHistory (update: String) {
-    historyArray.append(update)
-}
-
 class ViewController: UIViewController {
     @IBOutlet weak var currentValueLabel: UILabel!
-    
-    @IBOutlet weak var increaseValueButton: UIButton!
-    
-    @IBOutlet weak var decreaseValueButton: UIButton!
-    
-    @IBOutlet weak var resetButton: UIButton!
-    
     @IBOutlet weak var historyLog: UITextView!
     
+    //Значение счётчика
+    private var currentValue: Int = 0
+    //Массив куда вписывается журнал действий
+    private var historyArray: [String] = []
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        historyLog.text = "История изменений: \nПусто"
+    }
+    
+    //Отдаёт текущую дату и время
+    private func currentTime() -> String {
+        let time = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "[dd-MM-yyyy HH:mm:ss]"
+        let currentTime = dateFormatter.string(from: time)
+        return currentTime
+    }
+
+    //Добавляем историю в журнал
+    private func updateHistory (update: String) {
+        historyArray.append(update)
+    }
+
     //Обновить значение счётчика
-    func updateLabelText(){
+    private func updateLabelText() {
         currentValueLabel.text = "Значение счётчика: " + String(currentValue)
     }
     
     //Обновить журнал действий
-    func updateTextView(updating: String) -> Void{
-        var log: String = currentTime() + updating
+    private func updateTextView(updating: String) -> Void {
+        let log: String = currentTime() + updating
         updateHistory(update: log)
-        //historyLog.text = historyArray
-        var textForHistory: String = " "
-        var index = historyArray.count
-        while index > 0 {
-            textForHistory =  textForHistory + "\n" + historyArray[index - 1]
-            index -= 1
+        var textForHistory: String = ""
+        for item in historyArray.reversed() {
+            textForHistory = textForHistory + "\n" + item
         }
         historyLog.text = "История изменений:\n" + textForHistory
     }
@@ -70,32 +66,26 @@ class ViewController: UIViewController {
     @IBAction func increaseValue(_ sender: Any) {
         currentValue += 1
         updateLabelText()
-        updateTextView(updating: "\(loggingAction.increase.rawValue)")
-        
+        updateTextView(updating: "\(LoggingAction.increase.rawValue)")
     }
+    
     //Уменьшить значение счётчика
     @IBAction func decreaseValue(_ sender: Any) {
         // Проверка на отрицательное значение
         if currentValue > 0 {
             currentValue -= 1
             updateLabelText()
-            updateTextView(updating: "\(loggingAction.decrease.rawValue)")
+            updateTextView(updating: "\(LoggingAction.decrease.rawValue)")
         } else {
-            updateTextView(updating: "\(loggingAction.nagativeValue.rawValue)")
+            updateTextView(updating: "\(LoggingAction.nagativeValue.rawValue)")
         }
     }
+    
     // Сбросить значение счётчика
     @IBAction func resetValue(_ sender: Any) {
         currentValue = 0
         updateLabelText()
-        updateTextView(updating: "\(loggingAction.reset.rawValue)")
+        updateTextView(updating: "\(LoggingAction.reset.rawValue)")
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        historyLog.text = "История изменений: \nПусто"
-    }
 }
-
